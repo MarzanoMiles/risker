8fe101dae56e6e5e97375f85fe8fa6340ed13353
MarzanoMiles <166697214+MarzanoMiles@users.noreply.github.com>
2026-01-22 02:15:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const PORT = process.env.PORT || 3300;
 // Middleware
 app.use(cors({
   origin: [
-    'https://cofee-may.web.app/', 
-    'https://console.firebase.google.com/project/cofee-may/overview'
+    'https://cofee-may.web.app/'
   ],
   methods: ['GET', 'POST']
 }));
