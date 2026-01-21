a898f3426275f760b8c3323ed4290460771b4c46
MarzanoMiles <166697214+MarzanoMiles@users.noreply.github.com>
2026-01-22 01:43:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
