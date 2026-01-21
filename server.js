430c0c822810cbf5844c110fc155362699d9c3c0
MarzanoMiles <166697214+MarzanoMiles@users.noreply.github.com>
2026-01-22 02:16:33 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3300;
 // Middleware
 app.use(cors({
   origin: [
-    'https://cofee-may.web.app/'
+    'https://cofee-may.web.app'
   ],
   methods: ['GET', 'POST']
 }));
